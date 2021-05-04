40cfabf0dadac40edb06eac67ff078d9d1e7b24f
13samyakjain <samyakjain0113@gmail.com>
2021-05-05 00:10:38 +0530

    chamges

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/',(req,res)=>{
     return res.sendFile('index.html');
 })
